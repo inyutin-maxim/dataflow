943d9cc3b308f4ca8e3a18bb3badc32cca036a17
Stanislav Sidristy <sunex.development@gmail.com>
2018-05-04 00:33:36 +0300

    SlimLock struct

---
+++ Lifetime/Threading/SlimLock.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace System.Contracts.Threading
+{
+    public struct SlimLock
+    {
+        private int _ownerThreadId, _depth;
+
+        public bool TryEnter()
+        {
+            return TryEnter(Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public void Enter()
+        {
+            var currentThreadId = Thread.CurrentThread.GetHashCode();
+            while(!TryEnter(currentThreadId))
+            {
+                Thread.Yield();
+            }
+        }
+
+        public void Leave()
+        {
+            var currentThreadId = Thread.CurrentThread.GetHashCode();
+            if (_depth > 1 && _ownerThreadId == currentThreadId)
+            {
+                Interlocked.Decrement(ref _depth);
+            } 
+            if (_depth == 1 && Interlocked.CompareExchange(ref _ownerThreadId, 0, currentThreadId) != currentThreadId)
+            {
+                _depth = 0;
+                throw new Exception("Trying to leave from not entered block");
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool TryEnter(int currentThreadId)
+        {
+            if (Interlocked.CompareExchange(ref _ownerThreadId, currentThreadId, 0) == 0
+                || _ownerThreadId == currentThreadId)
+            {
+                Interlocked.Increment(ref _depth);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
